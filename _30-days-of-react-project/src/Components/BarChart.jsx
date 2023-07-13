db0ac32423fb172434a73478153a07fce831d829
catsAndIvy <bronwyn-elizabeth@live.co.uk>
2023-07-13 11:44:39 +0100

    add exercise solutions day06

--- _30-days-of-react-project/src/Components/BarChart.jsx
+++ _30-days-of-react-project/src/Components/BarChart.jsx
@@ -6,14 +6,15 @@ const BarChart = () => {
      
         <div className='header-wrapper'>
           <h2>Bar Chart</h2>
-
+          <h3 className="bar-title" >World Population</h3>
+          <p className="bar-explanation" >Ten most populated countries.</p>
           <ul>
           {tenHighestPopulation.map((country, index) => {
             return (
             <li key={index} className="single-country">
                 <p className="country-info" >{country.country.toUpperCase()}</p>
                 <div className="bar-chart" style={{ width: `${(country.population / 7693165599) * 100}%` }}></div>
-                <p className="country-info"> {country.population}</p>
+                <p className="country-info"> {country.population.toLocaleString()}</p>
             </li>)
           })}
           </ul>
