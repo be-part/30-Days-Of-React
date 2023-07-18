c16d49573b1fbfdde58e319e7fbad5c33b6e6943
catsAndIvy <bronwyn-elizabeth@live.co.uk>
2023-07-18 10:57:03 +0100

    add exercise solutions day17

---
+++ _solutions/day-17/exercises.js
@@ -0,0 +1,13 @@
+/* 
+What package do you use to implement routing in react?
+react-router-dom
+
+What is the default export in react-router-dom?
+BrowserRouter
+
+What is the use of the following Components(Route, NavLink, Redirect)?
+* Route: creates client side routing which allows your app to update the URL from a link click without making another request for another document from the server.
+* NavLink: Styling the active navigation items so the user knows where they are (isActive) or where they're going (isPending).
+* Redirect: While loading or changing data, it's common to redirect the user to a different route.
+
+*/
