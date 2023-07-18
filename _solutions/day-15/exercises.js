4c4f5e0f32c476e2482db729fdd435c480ea5267
catsAndIvy <bronwyn-elizabeth@live.co.uk>
2023-07-18 10:26:13 +0100

    add exercise solutions day15

---
+++ _solutions/day-15/exercises.js
@@ -0,0 +1,40 @@
+/* 
+What is a package?
+A package is a module that can be added to any program.
+
+What is a third party package ?
+Packages that come from another provider outside of React/the program, framework or library you are using.
+
+Do you have to use third party packages?
+No.
+
+How do you know the popularity and stability of a third party package?
+Check on npm.
+
+How many JavaScript packages are there on the npm registry?
+1.3 million.
+
+How do you install a third party package?
+npm install package_name
+
+What packages do you use most frequently?
+Jest, Supertest.
+
+What package do you use to fetch data?
+Axios.
+
+What is the purpose of classnames package?
+A simple JavaScript utility for conditionally joining classNames together.
+
+What is the pupose validator package?
+A way to validate various form inputs.
+
+
+Exercises: Level 2
+Learn how to use Sass. ✓
+
+Learn how to use axios. ✓
+
+Learn how to use moment and react-icons. ✓
+
+*/
