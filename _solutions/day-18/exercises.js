417dd07efe09adecb4a57eb4e407f75adce93faf
catsAndIvy <bronwyn-elizabeth@live.co.uk>
2023-07-18 11:14:49 +0100

    add exercise solutions day18

---
+++ _solutions/day-18/exercises.js
@@ -0,0 +1,22 @@
+/*
+Exercises: Level 1
+What is HTTP request?
+Is a request made by a client, to a named host, which is located on a server. 
+
+What are the most common HTTP requests?
+GET, POST, PUT, PATCH, DELETE
+
+What is fetch?
+JavaScript fetch API is a way of fetching resources from a server.
+
+What is axios?
+Axios is a promise-based HTTP Client that also fetches resources from a server, just a bit more efficient.
+
+What is the difference between fetch and axios?
+Axios more efficient and easier to use.
+
+Do you prefer fetch to axios for make HTTP requests?
+Axios.
+
+
+*/
