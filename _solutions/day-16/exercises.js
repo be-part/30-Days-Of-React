abe946beb690b56c4e8989b3a640bd35dc5dddcc
catsAndIvy <bronwyn-elizabeth@live.co.uk>
2023-07-18 10:36:41 +0100

    add exercise solutions day16

---
+++ _solutions/day-16/exercises.js
@@ -0,0 +1,18 @@
+/* 
+Exercises: Level 1
+
+What is higher order function?
+In JavaScript, a higher order function is a function that takes another function as a parameter or returns another function.
+
+What is Higher Order Component?
+A higher-order component is a function that takes a component and returns a new component.
+Example: react-router
+
+What is the difference between higher order function and higher order component?
+Higher order function takes a function and returns a function as opposed to a component in higher order components.
+
+A higher order component can allow us to enhance a component. (T or F)
+T
+Examples: loading, authentication.
+
+*/
